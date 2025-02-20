1f90be85bfb5d4bb48ba3d4faf8d356ce4fd124a
tk <dadakil83@gmail.com>
2025-02-20 01:38:26 -0800

    create semanticDetector.rs

---
+++ src/semanticDetector.rs
@@ -0,0 +1,11 @@
+use pyo3::prelude::*;
+use pyo3::types::PyList;
+use std::fs;
+
+fn find_semantic_duplicated_by_ai_model(&mut self) {
+    // compare ast for semantic
+    // compare the structures among functions
+    // then return the % 
+    // data flow approach or CFG?
+
+}
